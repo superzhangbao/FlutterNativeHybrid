e0d5099418095d16041e6e1d579b81e55ce85e9f
zhangbao <15056006309@163.com>
2020-04-27 01:44:07 +0800

    实现Flutter端与Native端通信的Flutter部分

--- lib_flutter/lib/main.dart
+++ lib_flutter/lib/main.dart
@@ -1,41 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp(initParams: window.defaultRouteName));
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final String initParams;
+
+  const MyApp({Key key, this.initParams}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter 混合开发',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter 混合开发',initParams:initParams),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  const MyHomePage({Key key, this.initParams, this.title}) : super(key: key);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  final String initParams;
   final String title;
 
   @override
@@ -43,68 +36,100 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  static const EventChannel _eventChannel = EventChannel("EventChannelPlugin");
+  static const MethodChannel _methodChannel =
+      MethodChannel("MethodChannelPlugin");
+  static const BasicMessageChannel<String> _basicMessageChannel =
+      const BasicMessageChannel("BasicMessageChannelPlugin", StringCodec());
+  String showMessage = "";
+  StreamSubscription _streamSubscription;
+  bool _isMethodChannel = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    _streamSubscription = _eventChannel
+        .receiveBroadcastStream("123")
+        .listen(_onToDart, onError: _onToDartError);
+    //使用BasicMessageChannel接受来自native的消息，并向native回复
+    _basicMessageChannel
+        .setMessageHandler((String message) => Future<String>(() {
+              setState(() {
+                showMessage = "BasicMessageChannel:" + message;
+              });
+              return "收到Native消息：" + message;
+            }));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        alignment: Alignment.topCenter,
+        decoration: BoxDecoration(color: Colors.lightBlueAccent),
+        margin: EdgeInsets.only(top: 70),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SwitchListTile(
+                value: _isMethodChannel,
+                onChanged: _onChangeChannel,
+                title: Text(_isMethodChannel?"MethodChannelPlugin" : "BasicMessageChannelPlugin"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            TextField(
+              onChanged: _onTextChange,
+              decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white)
+                ),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)
+                ),
+              ),
             ),
+            Text("收到初始化参数initParams:${widget.initParams}"),
+            Text("Native传来的数据:$showMessage"),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _onToDart(message) {
+    setState(() {
+      showMessage = message;
+    });
+  }
+
+  void _onToDartError(error) {
+    print(error);
+  }
+
+  void _onChangeChannel(bool value){
+    setState(() {
+      _isMethodChannel = value;
+    });
+  }
+
+  void _onTextChange(value) async {
+    String response;
+    try {
+      if(_isMethodChannel) {
+            //使用MessageChannel接受来自native的消息，并向native回复
+            response = await _methodChannel.invokeMethod("send",value);
+          }else{
+        response = await _basicMessageChannel.send(value);
+      }
+    } on PlatformException catch (e) {
+      print(e);
+    }
   }
 }
